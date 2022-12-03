f7d4efccd49839f39e1058eb50254e2ab90bef9d
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-12-03 11:37:08 -0500

    some minor changes to test comments

--- packages/type-filter/src/lib/primitive-filter/primitive-filter.function.spec.ts
+++ packages/type-filter/src/lib/primitive-filter/primitive-filter.function.spec.ts
@@ -1,14 +1,13 @@
 import { primitiveTypeFilter } from '../../../dist';
 
 describe('Tests for primitiveTypeFilter', () => {
-  const nums: number[] = [1, 2, 3, 4, 5];
-
   it('should exist', () => {
     expect(primitiveTypeFilter).toBeTruthy();
   });
 
-  it('Should be able to perform filtering on 1st level', () => {
+  it('Should be able to perform filtering of primitives', () => {
     // Arrange
+    const nums: number[] = [1, 2, 3, 4, 5];
 
     // Act
     const results = primitiveTypeFilter<number, 2>(nums, 2);
